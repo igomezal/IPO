0bebf57b2e5b21e08b2d827b12343469ced51290
Alvaro95pa <a.pavonal@alumnos.urjc.es>
2016-04-14 22:10:14 +0200

    Segundo actualización de usabilidad
    Modificadas las pantallas actividad y animales. Añadida la pantalla
    enunciado.

--- js/temas.js
+++ js/temas.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
     $('.ayuda').click(function(){
         $('.bocadillo').toggleClass('esconder');
     });
+    var fallos = 0;
     var cuenta = 1;
     $(document).on('click', '.ejercicio', function () {
         console.log('Pulsado');
@@ -18,14 +19,25 @@ $(document).ready(function(){
                 $('.respuesta').html('El <span style="color: blue">hipopóta_o</span> vive en África<span></span>');
             }else if(cuenta==4){
                 $('.respuesta').html('El <span style="color: blue">hipopótamo</span> vive en África<span></span>');
-                $('.fantastico').html('<center><img src="img/star.png" style="width: 40px; height: 40px;"> <img src="img/star.png" style="width: 40px; height: 40px;"> <img src="img/star_fallo.png" style="width: 40px; height: 40px;"><br><br><img src="img/logo-fantastico.png" style="width: 500px; height: 150px;"/><br><br><a type="button" href="index.html" class="btn btn-primary btn-lg glyphicon glyphicon-home"> Inicio</a>&nbsp &nbsp &nbsp<a type="button" href="actividad.html" class="btn btn-warning btn-lg glyphicon glyphicon-repeat"> Reiniciar</a></center>');
+                if(fallos == 0){
+                    $('.fantastico').html('<center><img src="img/star.png" style="width: 40px; height: 40px;"> <img src="img/star.png" style="width: 40px; height: 40px;"> <img src="img/star.png" style="width: 40px; height: 40px;"><br><br><img class="awesome" src="img/logo-fantastico.png"/><br><br><a type="button" href="temas.html" class="btn btn-primary btn-lg glyphicon glyphicon-book"> Temas</a>&nbsp &nbsp &nbsp<a type="button" href="actividad.html" class="btn btn-warning btn-lg glyphicon glyphicon-repeat"> Reiniciar</a></center>');
+                }
+                else if(fallos <= 5) {
+                    $('.fantastico').html('<center><img src="img/star.png" style="width: 40px; height: 40px;"> <img src="img/star.png" style="width: 40px; height: 40px;"> <img src="img/star_fallo.png" style="width: 40px; height: 40px;"><br><br><img class="awesome" src="img/logo-fantastico.png"/><br><br><a type="button" href="temas.html" class="btn btn-primary btn-lg glyphicon glyphicon-book"> Temas</a>&nbsp &nbsp &nbsp<a type="button" href="actividad.html" class="btn btn-warning btn-lg glyphicon glyphicon-repeat"> Reiniciar</a></center>');
+                }
+                else if(fallos > 5) {
+                    $('.fantastico').html('<center><img src="img/star.png" style="width: 40px; height: 40px;"> <img src="img/star_fallo.png" style="width: 40px; height: 40px;"> <img src="img/star_fallo.png" style="width: 40px; height: 40px;"><br><br><img class="awesome" src="img/logo-fantastico.png"/><br><br><a type="button" href="temas.html" class="btn btn-primary btn-lg glyphicon glyphicon-book"> Temas</a>&nbsp &nbsp &nbsp<a type="button" href="actividad.html" class="btn btn-warning btn-lg glyphicon glyphicon-repeat"> Reiniciar</a></center>');
+                }
+                
             }
             cuenta++;
         }else{
             $('#myError').modal('show');
-            
+            fallos += 1;
+            console.log(fallos);
         }
     });
+    
     $('#continuar').click(function(){
         $('#myError').modal('hide');
     });
