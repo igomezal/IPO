a9a64f808c500f105e0fcace8779583327868629
sergioserr <s.serranoserr@alumnos.urjc.es>
2016-04-21 12:55:53 +0200

    Añadido pequeñas mejoras

--- profesores/js/script.js
+++ profesores/js/script.js
@@ -1,11 +1,27 @@
 $(document).ready(function () {
     $('#enter').click(function () {
-        var nombre = $('#user_name').val();
-        var contraseña = $('#user_pass').val();
-        if(contraseña == 'pass' && nombre == 'profesor'){
-            window.open('home.html', '_self');
-        }else {
-            $('#error').html('Contraseña incorrecta');
+        entrar();
+    });
+    $('#user_name').keyup(function (event) {
+        if (event.which == 13) {
+            entrar();
+        }
+    });
+    $('#user_pass').keyup(function (event) {
+        if (event.which == 13) {
+            entrar();
         }
     });
 });
+
+var entrar = function(){
+    var nombre = $('#user_name').val();
+    var contraseña = $('#user_pass').val();
+    if(contraseña == 'pass' && nombre == 'profesor'){
+        window.open('home.html', '_self');
+    }else {
+        $('#error').html('Contraseña incorrecta <br><br>');
+    }
+}
+
+
